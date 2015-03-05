18a3d614bf074537040d33735e3d42ca4d479391
Matthias Rolke <mr.amtrack@gmail.com>
2015-03-05 22:05:55 +0100

    emit either mail or deletedMail event

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function Notifier(opts) {
         self.options.user = self.options.username;
     }
     self.options.box = self.options.box || 'INBOX';
+    self.options.emitOnStartup = self.options.emitOnStartup || false;
+    self.options._maxUpdateSize = self.options._maxUpdateSize || 20;
     self.hideLogs = !!self.options.hideLogs;
     self.connected = false;
     self.imap = new Imap(opts);
@@ -25,6 +27,10 @@ function Notifier(opts) {
     self.imap.on('error', function (err) {
         self.emit('error', err);
     });
+    self.cache = {
+        uidlist: []
+    };
+    self.seqno2mid = {};
 }
 util.inherits(Notifier, EventEmitter);
 
@@ -38,9 +44,12 @@ Notifier.prototype.start = function () {
     self.imap.once('ready', function () {
         self.connected = true;
         self.imap.openBox(self.options.box, false, function () {
-            self.scan();
+            self.scan(self.options.emitOnStartup);
             self.imap.on('mail', function (id) {
-                self.scan();
+                self.scan(true);
+            });
+            self.imap.on('expunge', function (id) {
+                self.scan(true);
             });
         });
     });
@@ -48,12 +57,45 @@ Notifier.prototype.start = function () {
     return this;
 };
 
-Notifier.prototype.scan = function () {
+Notifier.prototype.scan = function (notifyNew) {
     var self = this;
+    var cached = self.cache;
     self.imap.search(self.options.search || ['UNSEEN'], function (err, seachResults) {
+        var deltaNew, deltaDeleted, batch;
         if (err) {
             self.emit('error', err);
         }
+        // normalize the uidlist
+        cached.uidlist = cached.uidlist || [];
+
+        // determine deleted uids
+        deltaDeleted = cached.uidlist.filter(function(i) {
+            return seachResults.indexOf(i) < 0;
+        });
+
+        // notify about deleted messages
+        if (deltaDeleted.length) {
+            for (var i=0; i < deltaDeleted.length; i++) {
+                var m = self.seqno2mid[deltaDeleted[i]];
+                if (m) {
+                    self.emit('deletedMail', m)
+                }
+            }
+        }
+        deltaNew = seachResults.filter(function(i) {
+            return cached.uidlist.indexOf(i) < 0;
+        }).sort(function(a, b) {
+            return b - a;
+        }));
+
+         // notify about new messages in batches of options._maxUpdateSize size
+        while (deltaNew.length) {
+            batch = deltaNew.splice(0, (self.options._maxUpdateSize || deltaNew.length));
+        }
+
+        // update mailbox info
+        cached.uidlist = seachResults;
+
         if (!seachResults || seachResults.length === 0) {
             if(!self.options.hideLogs) {
                 util.log('no new mail in ' + self.options.box);
@@ -64,10 +106,20 @@ Notifier.prototype.scan = function () {
             markSeen: self.options.markSeen !== false,
             bodies: ''
         });
-        fetch.on('message', function (msg) {
+        fetch.on('message', function (msg, seqno) {
+            var newSeqNo = parseInt(seqno) + parseInt(batch) - 1;
             var mp = new MailParser();
             mp.once('end', function (mail) {
-                self.emit('mail', mail);
+                self.seqno2mid[newSeqNo] = {
+                    headers: {
+                        'message-id': mail.headers['message-id'],
+                        subject: mail.headers.subject,
+                        from: mail.headers.from
+                    }
+                };
+                if (notifyNew) {
+                    self.emit('mail', mail);
+                }
             });
             msg.once('body', function (stream, info) {
                 stream.pipe(mp);
---
+++ sample/new-deleted-mail-notifier.js
@@ -0,0 +1,18 @@
+var notifier = require('../index.js');
+
+var imap = {
+  user: "jerome.creignou",
+  password: "password",
+  host: "imap.host.com",
+  port: 993, // imap port
+  tls: true,// use secure connection
+  tlsOptions: { rejectUnauthorized: false }
+};
+
+notifier(imap).on('mail',function(mail){
+  console.log(mail);
+})
+.on('deletedMail', function(mail){
+  console.error(mail);
+})
+.start();
